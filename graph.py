79e424c9faffc84e0bca48d86e32decd26b03c51
vowchick <37148723+vowchick@users.noreply.github.com>
2021-03-21 12:21:47 +0300

    NEW: plot signal

---
+++ graph.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+def draw_plot (signal):
+    plt.plot (signal)
+    plt.show ()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import aio
+import graph
 import sys
 filename = "audio_files/test_file.wav"
 filenameshort = "audio_files/short_file.wav"
@@ -6,7 +7,8 @@ filename_out = "audio_files/out.wav"
 
 if __name__ == '__main__':
     signal, sample_rate = aio.read_signal (filename)
+    graph.draw_plot (signal)
 
-    aio.write_signal (filename_out, signal, sample_rate)
-    aio.play_sound (filename_out)
+#    aio.write_signal (filename_out, signal, sample_rate)
+#    aio.play_sound (filename_out)
 
