cf0aa7102aa669733e929ddce1fb5b6155d170b1
vowchick <37148723+vowchick@users.noreply.github.com>
2021-03-28 19:24:18 +0300

    REF: make third tasks follow convention

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ import graph
 import sys
 import processing as pc
 import tasks
-filename = "audio_files/test_file.wav"
-filenameshort = "audio_files/short_file.wav"
-filename_out = "audio_files/out.wav"
-images_dir = "images/"
+im_dir = "images/"
+audio_dir = "audio_files/"
+filename = audio_dir + "test_file.wav"
+filenameshort = audio_dir +  "short_file.wav"
+filename_out = audio_dir + "out.wav"
 ndft = 256
 noverlap = 84
+images_dir_message = "(You can look at signal plots in directory %s)" % (im_dir)
+audio_dir_message = "(You can listen to original signal and signal with squeak in directory %s)" % (audio_dir)
 
 
 if __name__ == '__main__':
     signal, sample_rate = tasks.first (filenameshort)
     tasks.done (1)
-    tasks.second (signal, sample_rate, 0.1, filename_out, images_dir)
-    tasks.done (2, "(You can look at signal plots in directory %s)" % (images_dir))
 
+    tasks.second (signal, sample_rate, 0.1, filename_out)
+    tasks.done (2, images_dir_message +"\n" +  audio_dir_message)
 
-    starts, spec = pc.make_spectrogramm (signal, ndft, noverlap)
-    graph.plot_spectrogramm (spec, sample_rate, len (signal), starts)
-    graph.save (images_dir + "spectrogramm")
+    tasks.third(signal, ndft, noverlap, sample_rate)
+    tasks.done (3, images_dir_message)
--- tasks.py
+++ tasks.py
@@ -2,17 +2,22 @@ import graph
 import processing as pc
 import aio
 
+images_dir = "images/"
+
 def done (number, message = ""):
     print (number, "task finished", message)
 def first (f_name):
     return aio.read_signal (f_name)
 
-def second (signal, sample_rate, part, f_out, i_dir):
+def second (signal, sample_rate, part, f_out):
     sin_signal = pc.get_sin (signal, sample_rate, part)
     aio.write_signal (f_out, sin_signal + signal, sample_rate)
-    aio.play_sound (f_out)
-    graph.plot_second_task_signals (signal, sin_signal, i_dir)
+    #aio.play_sound (f_out)
+    graph.plot_second_task_signals (signal, sin_signal, images_dir)
 
 
 
-
+def third (signal, ndft, noverlap, sample_rate):
+    starts, spec = pc.make_spectrogramm (signal, ndft, noverlap)
+    graph.plot_spectrogramm (spec, sample_rate, len (signal), starts)
+    graph.save (images_dir + "spectrogramm")
